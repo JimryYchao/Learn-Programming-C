e37c7dd31879cf26c7e60e8344853632ded719ae
JimryYchao <17889982535@163.com>
2023-07-17 18:39:25 +0800

    update clib

--- _03_C 标准库参考/clib-test/s_ctype.c
+++ _03_C 标准库参考/clib-test/s_ctype.c
@@ -1,13 +1,34 @@
 #include "_test_head.h"
-#include "s_ctype.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+// REGION: file Character classification functions
+
+#define m_isalnum(C) isalnum(C)   // 是否是字母或数字
+#define m_isalpha(C) isalpha(C)   // 是否是英文字母
+#define m_islower(C) islower(C)   // 是否是小写字母
+#define m_isupper(C) isupper(C)   // 是否是大写字母
+#define m_isdigit(C) isdigit(C)   // 是否为数字
+#define m_isxdigit(C) isxdigit(C) // 是否是十六进制的字符
+#define m_iscntrl(C) iscntrl(C)   // 是否是控制字符
+#define m_isgraph(C) isgraph(C)   // 是否是可显示的字符
+#define m_isspace(C) isspace(C)   // 是否是空白字符
+#define m_isblank(C) isblank(C)   // 是否是空格字符
+#define m_isprint(C) isprint(C)   // 是否是可打印字符
+#define m_ispunct(C) ispunct(C)   // 是否是一个标点符号
+#define m_isascii(C) isascii(C)   // 是否是 ASCII 字符
+
+// REGION: Character case mapping functions
+#define m_tolower(C) tolower(C) // 将大写字母转换为对应的小写字母
+#define m_toupper(C) toupper(C) // 将小写字母转换为对应的大写字母
 
 #define ISLOWER(c) ('a' <= (c) && (c) <= 'z')
 #define TOUPPER(c) (ISLOWER(c) ? 'A' + ((c) - 'a') : (c))
 #define XOR(e, f) (((e) && !(f)) || (!(e) && (f)))
 
 static void Test_Ctype_functions();
+static void Test_Wctype();
 inline static void print_char(unsigned char c)
 {
     printf("%d/", (int)c);
@@ -20,6 +41,8 @@ inline static void print_char(unsigned char c)
 void Test_C_Lib()
 {
     Test_Ctype_functions();
+
+    Test_Wctype();
 }
 
 void Test_Ctype_functions()
@@ -89,4 +112,20 @@ int main(int argc, char const *argv[])
 {
     Test_C_Lib();
     return 0;
+}
+
+//---------------------------------------------------
+#include <stdio.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <locale.h>
+
+void Test_Wctype()
+{
+    wchar_t c = L'\u2002'; // Unicode 字符 'EN SPACE'
+    printf("in the default locale, iswprint(%#x) = %d\n", c, !!iswprint(c));
+    setlocale(LC_ALL, "en_US.utf8");
+    printf("in Unicode locale, iswprint(%#x) = %d\n", c, !!iswprint(c));
+    wchar_t c2 = L'\x82'; // 容许的打断
+    printf("in Unicode locale, iswprint(%#x) = %d\n", c2, !!iswprint(c2));
 }
---
+++ _03_C 标准库参考/clib-test/s_errno.c
@@ -0,0 +1,63 @@
+#include "_test_head.h"
+#include <errno.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+static void Test_errno();
+static void Test_Macros();
+
+void Test_C_Lib()
+{
+  Test_errno();
+}
+
+void Test_errno()
+{
+  errno = 0;
+  1.0 / 0.0;
+  Test_Macros();
+
+  errno = 0;
+  acos(+1.1);
+  Test_Macros();
+
+  errno = 0;
+  log(0.0);
+  Test_Macros();
+
+  errno = 0;
+  sin(0.0);
+  Test_Macros();
+}
+void Test_Macros()
+{
+  const char *err_info = "unknown error";
+  switch (errno)
+  {
+  case EDOM:
+    err_info = "domain error";
+    break;
+  case EILSEQ:
+    err_info = "illegal sequence";
+    break;
+  case ERANGE:
+    err_info = "pole or range error";
+    break;
+  case 0:
+    err_info = "no error";
+  }
+  fputs(err_info, stdout);
+  puts(" occurred");
+}
+
+int main(void)
+{
+  Test_C_Lib();
+}
+/*
+  no error occurred
+  domain error occurred
+  pole or range error occurred
+  no error occurred
+*/
