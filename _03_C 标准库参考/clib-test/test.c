280d818a26bf2787a061d063bc16966a65254757
JimryYchao <17889982535@163.com>
2023-09-27 04:39:18 +0800

    add: stdlib

--- _03_C 标准库参考/clib-test/test.c
+++ _03_C 标准库参考/clib-test/test.c
@@ -1,21 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1 // 0
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+#include <locale.h>
 
-int n;
+void demo(wchar_t wc)
+{
+	printf("State-dependent encoding?   %d\n", wctomb(NULL, wc));
+	char mb[512];
+	int len = wctomb(mb, wc);
+	if (len > 0)
+	{
+		wprintf(L"wide char '%lc' -> multibyte char '", wc);
+		for (int idx = 0; idx < len; ++idx)
+			printf("%#2x ", (unsigned char)mb[idx]);
+		printf("'\n");
+	}
+}
 
 int main(void)
 {
-	FILE *f = fopen("test.txt", "a");
-	char s[] = " world";
-	fwrite(s, 1, sizeof s - 1, f);
-
-	if (freopen("test.txt", "rb", f))
-	{
-		char buf[BUFSIZ] = {0};
-		fread(buf, 1L, BUFSIZ - 1, f);
-		printf("%s\n", buf);
-	}
-	else
-		perror("e");
-	fclose(f);
+	setlocale(LC_ALL, "en_US.utf8");
+	printf("MB_CUR_MAX = %zu\n", MB_CUR_MAX);
+	demo(L'A');
+	demo(L'\u00df');
+	demo(L'\U0001d10b'); // 过大
 }
