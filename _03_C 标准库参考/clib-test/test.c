f849944c62c924d9e742b65c42bcf05e7947f15c
JimryYchao <17889982535@163.com>
2024-02-05 01:11:53 +0800

    delete src glic

--- _03_C 标准库参考/clib-test/test.c
+++ _03_C 标准库参考/clib-test/test.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "test.h"
+#include <stdbool.h>
+#include <tchar.h>
+
+typedef struct{
+    int len;
+    int arr[];
+} Sample;
 
 int main(int argc, char *argv[], char *envp[])
 {
     puts("Hello");
-    _BitInt(10);
+
+    Sample arr[] = {1,2,3,4};
+    struct S
+    {
+        int value;
+        int id;
+    };
+
+    printf("%d", num);
 }
