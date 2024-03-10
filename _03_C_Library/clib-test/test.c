65117f030961e5bd8eaf02c0353d97fe13217325
JimryYchao <17889982535@163.com>
2024-03-10 09:44:02 +0800

    add { }

--- _03_C_Library/clib-test/test.c
+++ _03_C_Library/clib-test/test.c
@@ -1,14 +1,6 @@
-#define __STDC_WANT_IEC_60559_EXT__
-#include <math.h>
-#include <float.h>
 #include <stdio.h>
-#include <stdatomic.h>
-#include <time.h>
 
 int main()
 {
-    intptr_t
-        printf("%LD", __DEC128_MAX__);
+     printf("Hello");
 }
-
-// (X x (1+0.18+0.053+0.111+0.111 0.041 + 0.466+(0.48))) + extATK x (1.466) x (2.008)
